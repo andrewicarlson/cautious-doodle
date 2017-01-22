b201e1c3fac66144defb18a839ba35ce0816b249
Andrew Carlson <andrew@bubinga.co>
2017-01-22 11:27:23 -0600

    Updates the description with a general flow of what happens on button press.

--- src/components/DemoDescription.vue
+++ src/components/DemoDescription.vue
@@ -5,6 +5,14 @@
         This demo is a POC showing the flow of information from a front end
         application to a back end microservice on AWS.
       </p>
+      <h3 class="md-headline">Steps</h3>
+      <ol>
+        <li>'Make Request' button is pressed.</li>
+        <li>GET request is sent to an endpoint on Amazon API Gateway.</li>
+        <li>The API Gateway routes the request to a Lambda expression.</li>
+        <li>The Lambda expression inserts a record into DynamoDB.</li>
+        <li>The insertion stream is read from DynamoDB and the result is displayed in the cards to the right.</li>
+      </ol>
     </div>
     <md-button v-on:click="makeRequest" class="md-raised md-primary">Make Request</md-button>
   </section>
@@ -15,7 +23,6 @@ export default {
   name: 'demo-description',
   methods: {
     makeRequest: function makeRequest() {
-      console.log(this);
     },
   },
 };
